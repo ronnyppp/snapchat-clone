13feddb0651322e28852e48e61e765a4142f8a2f
ronnyppp <123583213+ronnyppp@users.noreply.github.com>
2026-04-28 21:22:09 -0400

    Removed unnecesarry imports

--- app/src/main/java/com/example/snapchatclone/TopBar.kt
+++ app/src/main/java/com/example/snapchatclone/TopBar.kt
@@ -4,10 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
